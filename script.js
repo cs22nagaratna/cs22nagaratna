44836b7792215b112d0aabb3c06db87b9e6913db
cs22nagaratna <cs22nagaratna@svpkumta.in>
2024-11-07 10:39:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function validate(){
+    let email=document.getElementById("email").value
+    let password=document.getElementById("password").value
+    if(email==="svpkumta@123" && password==="123"){
+        alert("Login successful")
+
+    }
+    else{
+        alert("Login faild")
+    }
+
+}
